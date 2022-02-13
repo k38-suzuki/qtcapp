1a3ccb732cfeaae23e528e637cfae6254411839c
k38-suzuki <suzuki.kenta38@jaea.go.jp>
2022-02-13 14:42:34 -0800

    fix script

--- misc/script/uninstall.sh
+++ misc/script/uninstall.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 sudo rm /usr/local/bin/qtcapp
-sudo rm /usr/local/include/App.h
 sudo rm /usr/local/lib/libQtcBase.so
+sudo rm -rf /usr/local/include/qtc
 sudo rm -rf /usr/local/lib/cmake
-
