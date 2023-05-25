f60e01fd34bedeaf7839d2335c69a33dbaf53bc3
k38-suzuki <suzuki.kenta38@jaea.go.jp>
2023-05-25 19:21:24 +0900

    Add break

--- src/qtcapp_node.cpp
+++ src/qtcapp_node.cpp
@@ -11,10 +11,12 @@ using namespace qtcapp;
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+
     MainWindow w;
     for(int i = 0; i < argc; ++i) {
         w.load(QString(argv[i]).toStdString());
     }
     w.show();
+
     return app.exec();
 }
